ef5330a87285515bbf53e78084b88b806749826a
Vadim <bekmansurov700@mail.ru>
2023-05-03 15:26:40 +0500

    added sayHello task

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -1,17 +1,15 @@
-/**
- * Эту функцию трогать не нужно
- */
+
 function print(text) {
   console.log(text);
 }
 
-/**
- * Эту функцию нужно поменять так,
- * чтобы функция sayHello работала корректно
- */
 function isValid(name) {
-  // ваш код...
+  if ((name !== null) && (name.length >= 4) && (!name.includes(' '))) {
+    return true;
+  }
+  return false;
 }
+
 
 function sayHello() {
   let userName = prompt('Введите ваше имя');
