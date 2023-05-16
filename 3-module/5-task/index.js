abceb4b9dffd0b132fb482aceb9515230b7bbc23
Vadim <bekmansurov700@mail.ru>
2023-05-16 16:41:37 +0500

    fixed 5th task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -9,8 +9,8 @@ function getMinMax(str) {
   // const min = array.reduce((current, next) => current < next ? current : next);
   // const max = array.reduce((current, next) => current > next ? current : next);
 
-  const min = Math.min.apply(null, array);
-  const max = Math.max.apply(null, array);
+  const min = Math.min(...array);
+  const max = Math.max(...array);
 
   result.min = min;
   result.max = max;
